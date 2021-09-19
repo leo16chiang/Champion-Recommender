a1223e9ec3a96e47bc6c3d05df591e925f443659
Leo <leo16chiang@gmail.com>
2021-09-18 23:48:42 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # MyShop
 Using Data Science to Guess League of Legends My Shop Skins
-d
-d
-d
-d
-d
-d
-d
-d
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # Use League of Legends API to get match history of a username
 
+import urllib.request
+from urllib.request import urlopen
+import json
+
+#Riot Games API Key
+key = RGAPI-ced52b64-2200-4651-b293-e1352c0302b4
